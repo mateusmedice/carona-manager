3560fd41628257501df54b9974bf43f7eac86699
Mateus Medice <mateus.medice@gmail.com>
2014-02-09 15:12:37 -0200

    Ajustes.

--- carona-manager-server/src/br/com/carona/manager/webservice/CityWebService.java
+++ carona-manager-server/src/br/com/carona/manager/webservice/CityWebService.java
@@ -1,7 +1,5 @@
 package br.com.carona.manager.webservice;
 
-import java.util.List;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -9,18 +7,21 @@ import javax.ws.rs.Produces;
 
 import br.com.carona.manager.business.CityBusiness;
 import br.com.carona.manager.business.impl.CityBusinessImpl;
-import br.com.carona.manager.model.City;
+
+import com.google.gson.Gson;
 
 @Path("/city")
 public class CityWebService {
 	
 	@GET
 	@Path("/{uf}")
-	@Produces("text/xml")
-	public List<City> getCityByState(@PathParam("uf") String uf) {
+	@Produces("application/json")
+	public String getCityByState(@PathParam("uf") String uf) {
 
 		CityBusiness cityBusiness = new CityBusinessImpl();
 		
-		return cityBusiness.getByState(uf);
+		Gson gson = new Gson();
+		
+		return gson.toJson(cityBusiness.getByState(uf));
 	}
 }
