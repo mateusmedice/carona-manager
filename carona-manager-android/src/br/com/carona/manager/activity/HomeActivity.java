faf53b56bac6dc6036140ee281e473e87e9eab82
Léo <leo.generoso@hotmail.com>
2014-02-10 20:34:57 -0200

    criacao da home page.

---
+++ carona-manager-android/src/br/com/carona/manager/activity/HomeActivity.java
@@ -0,0 +1,22 @@
+package br.com.carona.manager.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class HomeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+}
