e162bfe51df7cbf5f9234e57b4893bf9f47769ed
Mateus Medice <mateus.medice@gmail.com>
2014-05-21 10:27:36 -0300

    saasdasda

--- carona-manager-android/src/br/com/carona/manager/activity/util/Constants.java
+++
@@ -1,9 +0,0 @@
-package br.com.carona.manager.activity.util;
-
-public class Constants {
-
-	public static final String WS_SERVER_ADDRESS = "http://localhost:8080/";
-
-	public static final String WS_SERVER_CONTEXT = "carona-manager-server/";
-	
-}
